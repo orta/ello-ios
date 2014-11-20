1cefc9f51254d4abffef4cf3a8a45c1f7e70b681
Sean Dougherty <seancdougherty@gmail.com>
2014-11-20 11:16:53 -0700

    Initial Commit

---
+++ Ello/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Ello
+//
+//  Created by Sean Dougherty on 11/20/14.
+//  Copyright (c) 2014 Ello. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
