588240cdc49eeb75528a310111ba37bf19cbb8ca
Sean Dougherty <seancdougherty@gmail.com>
2015-02-04 13:00:57 -0700

    Parse assets from linked dictionary.

--- Sources/Model/Block.swift
+++ Sources/Model/Block.swift
@@ -23,7 +23,7 @@ class Block {
         self.kind = kind
     }
     
-    class func blocks(json:JSON) -> [Block] {
+    class func blocks(json:JSON, assets:[String: AnyObject]?) -> [Block] {
         let content = json["content"].object as [AnyObject]
         return content.map { (contentDict) -> Block in
             let kind = Block.Kind(rawValue: contentDict["kind"] as String) ?? Block.Kind.Unknown
@@ -36,12 +36,56 @@ class Block {
                 let data = data as [String:AnyObject]
                 let alt = data["alt"] as? String ?? ""
                 let url = data["url"] as String
-                return ImageBlock(alt: alt, url: NSURL(string: url)!)
+                let assetId = data["asset_id"] as? String
+
+                let imageBlock = ImageBlock(alt: alt, assetId: assetId, url: NSURL(string: url)!)
+
+                if let assets = assets {
+                    if let assetId = assetId {
+                        println("assetId = \(assetId)")
+                        if let attachment = assets[assetId]?["attachment"] as? [String:AnyObject] {
+                            let hdpi = attachment["hdpi"] as? [String:AnyObject]
+                            if let hdpi = hdpi {
+                                let hdpiAttachment =
+                                ImageAttachment(
+                                    url: NSURL(string: hdpi["url"] as String),
+                                    height: hdpi["metadata"]?["height"] as? Int,
+                                    width: hdpi["metadata"]?["width"] as? Int,
+                                    imageType: hdpi["metadata"]?["type"] as? String,
+                                    size: hdpi["metadata"]?["size"] as? Int)
+                                imageBlock.hdpi = hdpiAttachment
+                            }
+
+                            let xxhdpi = attachment["xxhdpi"] as? [String:AnyObject]
+
+                            if let xxhdpi = xxhdpi {
+                                let xxhdpiAttachment =
+                                ImageAttachment(
+                                    url: NSURL(string: xxhdpi["url"] as String),
+                                    height: xxhdpi["metadata"]?["height"] as? Int,
+                                    width: xxhdpi["metadata"]?["width"] as? Int,
+                                    imageType: xxhdpi["metadata"]?["type"] as? String,
+                                    size: xxhdpi["metadata"]?["size"] as? Int)
+                                imageBlock.xxhdpi = xxhdpiAttachment
+                            }
+                        }
+                    }
+                }
+
+                return imageBlock
             case .Unknown:
                 return UnknownBlock()
             }
         }
     }
+}
+
+struct ImageAttachment {
+    let url: NSURL?
+    let height: Int?
+    let width: Int?
+    let imageType: String?
+    let size: Int?
 }
 
 class UnknownBlock : Block {
@@ -51,10 +95,14 @@ class UnknownBlock : Block {
 }
 
 class ImageBlock : Block {
+    let assetId:String?
     let alt:String
     let url:NSURL?
+    var hdpi:ImageAttachment?
+    var xxhdpi:ImageAttachment?
     
-    init(alt: String, url:NSURL?) {
+    init(alt: String, assetId: String?, url:NSURL?) {
+        self.assetId = assetId
         self.alt = alt
         self.url = url
         super.init(kind: Block.Kind.Image)
--- Sources/Model/User.swift
+++ Sources/Model/User.swift
@@ -19,12 +19,11 @@ class User: JSONAble {
     let experimentalFeatures: Bool
     let relationshipPriority: String
     let avatarURL: NSURL?
-    let pixellatedAvatarURL: NSURL?
     let followersCount: Int?
     let postsCount: Int?
     let followingCount: Int?
 
-    init(name: String, userId: String, username: String, avatarURL: NSURL?, pixellatedAvatarURL: NSURL?, experimentalFeatures: Bool, href:String, relationshipPriority:String, followersCount:Int?, postsCount:Int?, followingCount:Int?) {
+    init(name: String, userId: String, username: String, avatarURL: NSURL?, experimentalFeatures: Bool, href:String, relationshipPriority:String, followersCount:Int?, postsCount:Int?, followingCount:Int?) {
         self.name = name
         self.userId = userId
         self.username = username
@@ -35,7 +34,6 @@ class User: JSONAble {
         self.followersCount = followersCount
         self.followingCount = followingCount
         self.postsCount = postsCount
-        self.pixellatedAvatarURL = pixellatedAvatarURL
     }
 
     override class func fromJSON(data:[String: AnyObject]) -> JSONAble {
@@ -44,28 +42,24 @@ class User: JSONAble {
         let name = json["name"].stringValue
         let userId = json["id"].stringValue
         let username = json["username"].stringValue
-//        var avatar = json["avatar"].object as [String:[String:String]]
+
 
         let experimentalFeatures = json["experimental_features"].boolValue
         let href = json["href"].stringValue
         let relationshipPriority = json["relationship_priority"].stringValue
 
-//        var avatarPath = avatar["large"]?["url"]
         var avatarURL:NSURL?
-//        if let avatarPath = avatarPath {
-//            avatarURL = NSURL(string: avatarPath, relativeToURL: NSURL(string: "https://ello.co"))
-//        }
 
-//        var pixellatedAvatarPath = avatar["pixellated_large"]?["url"]
-        var pixellatedAvatarURL:NSURL?
-//        if let pixellatedAvatarPath = pixellatedAvatarPath {
-//            pixellatedAvatarURL = NSURL(string: pixellatedAvatarPath, relativeToURL: NSURL(string: "https://ello.co"))
-//        }
+        if var avatar = json["avatar"].object as? [String:[String:AnyObject]] {
+            if let avatarPath = avatar["large"]?["url"] as? String {
+                avatarURL = NSURL(string: avatarPath, relativeToURL: NSURL(string: "https://ello.co"))
+            }
+        }
 
         let postsCount = json["posts_count"].int
         let followersCount = json["followers_count"].int
         let followingCount = json["following_count"].int
         
-        return User(name: name, userId: userId, username: username, avatarURL:avatarURL, pixellatedAvatarURL:pixellatedAvatarURL, experimentalFeatures: experimentalFeatures, href:href, relationshipPriority:relationshipPriority, followersCount: followersCount, postsCount: postsCount, followingCount:followingCount)
+        return User(name: name, userId: userId, username: username, avatarURL:avatarURL, experimentalFeatures: experimentalFeatures, href:href, relationshipPriority:relationshipPriority, followersCount: followersCount, postsCount: postsCount, followingCount:followingCount)
     }
 }
