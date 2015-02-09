7cbc333556c91a456c20a274fd7063335e65fa19
Colin T.A. Gray <colinta@gmail.com>
2015-02-09 16:10:52 -0700

    some more notification code while i check out sd-specs

--- Sources/Controllers/Notifications/NotificationsViewController.swift
+++ Sources/Controllers/Notifications/NotificationsViewController.swift
@@ -18,6 +18,30 @@ class NotificationsViewController: BaseElloViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBarHidden = true
+
+        setupStreamController()
+    }
+
+    private func setupStreamController() {
+        let controller = StreamViewController.instantiateFromStoryboard()
+        controller.streamKind = .Notifications
+        controller.postTappedDelegate = self
+
+        let streamService = StreamService()
+        streamService.loadloadNotificationsStream(
+            success: { (streamables) -> () in
+                controller.addStreamables(streamables)
+                controller.doneLoading()
+            },
+            failure: { (error, statusCode) -> () in
+                println("failed to load notifications")
+                controller.doneLoading()
+            }
+        )
+
+        controller.willMoveToParentViewController(self)
+        self.view.addSubview(controller.view)
+        self.addChildViewController(controller)
     }
 
 }
