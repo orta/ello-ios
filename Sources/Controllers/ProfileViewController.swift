1f1ae34ea641a5a193cca0da1b9a55d5865a5450
Sean Dougherty <seancdougherty@gmail.com>
2015-02-14 01:02:28 -0700

    Stretch cover header image when scrolling.

--- Sources/Controllers/ProfileViewController.swift
+++ Sources/Controllers/ProfileViewController.swift
@@ -12,8 +12,13 @@ class ProfileViewController: StreamableViewController {
 
     let user: User
     let streamViewController: StreamViewController
+    var coverImageHeightStart: CGFloat?
+    var coverWidthSet = false
+    let ratio:CGFloat = 16.0/9.0
+
     @IBOutlet weak var viewContainer: UIView!
     @IBOutlet weak var coverImage: UIImageView!
+    @IBOutlet weak var coverImageHeight: NSLayoutConstraint!
 
     required init(user : User) {
         self.user = user
@@ -27,14 +32,13 @@ class ProfileViewController: StreamableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        coverImage.alpha = 0
         if user.isCurrentUser {
             // do stuff
         }
 
         let item = UIBarButtonItem.backChevronWithTarget(self, action: "backTapped:")
         navigationItem.leftBarButtonItem = item
-
         setupStreamController()
     }
 
@@ -42,6 +46,14 @@ class ProfileViewController: StreamableViewController {
         NSNotificationCenter.defaultCenter().postNotificationName(AccessManager.Notifications.LoggedOut.rawValue, object: nil)
     }
 
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        if !coverWidthSet {
+            coverWidthSet = true
+            coverImageHeight.constant = view.frame.width / ratio
+            coverImageHeightStart = coverImageHeight.constant
+        }
+    }
 
     private func setupStreamController() {
         StreamService().loadUser(streamViewController.streamKind.endpoint,
@@ -50,7 +62,7 @@ class ProfileViewController: StreamableViewController {
                 println("failed to load user (reason: \(error))")
                 self.streamViewController.doneLoading()
             })
-
+        streamViewController.streamScrollDelegate = self
         streamViewController.willMoveToParentViewController(self)
         viewContainer.addSubview(streamViewController.view)
         streamViewController.view.frame = viewContainer.bounds
@@ -61,7 +73,12 @@ class ProfileViewController: StreamableViewController {
 
     private func userLoaded(user: User) {
         if let cover = user.coverImageURL {
-            coverImage.sd_setImageWithURL(cover)
+            coverImage.sd_setImageWithURL(cover, completed: {
+                (image, error, type, url) in
+                UIView.animateWithDuration(0.15, animations: {
+                    self.coverImage.alpha = 1.0
+                })
+            })
         }
         
         let profileHeaderCellItem = StreamCellItem(jsonable: user, type: StreamCellType.ProfileHeader, data: nil, oneColumnCellHeight: 320.0, multiColumnCellHeight: 0.0, isFullWidth: true)
@@ -70,5 +87,14 @@ class ProfileViewController: StreamableViewController {
         var parser = StreamCellItemParser()
         streamViewController.addUnsizedCellItems(parser.postCellItems(user.posts))
         streamViewController.doneLoading()
+    }
+}
+
+extension ProfileViewController: StreamScrollDelegate {
+
+    func scrollViewDidScroll(scrollView : UIScrollView) {
+        if let (start, width) = unwrap(coverImageHeightStart, coverImage.image?.size.width) {
+            coverImageHeight.constant = max(start - scrollView.contentOffset.y, start)
+        }
     }
 }
