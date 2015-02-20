d9b6f9a40f251d2289803b774c781174e309859a
Colin T.A. Gray <colinta@gmail.com>
2015-02-19 21:47:47 -0700

    refactored Colors to memoize values and added some notes

--- Sources/Styling/Colors.swift
+++ Sources/Styling/Colors.swift
@@ -8,20 +8,47 @@
 
 import UIKit
 
+private struct ElloColors {
+    static let grey231F20   : UIColor = UIColor(hex: 0x231F20)
+    static let grey3        : UIColor = UIColor(hex: 0x333333)
+    static let grey4D       : UIColor = UIColor(hex: 0x4D4D4D)
+    static let grey6        : UIColor = UIColor(hex: 0x666666)
+    static let greyA        : UIColor = UIColor(hex: 0xAAAAAA)
+    static let greyE5       : UIColor = UIColor(hex: 0xE5E5E5)
+    static let greyF1       : UIColor = UIColor(hex: 0xF1F1F1)
+    static let yellowFFFFCC : UIColor = UIColor(hex: 0xFFFFCC)
+    static let redFFCCCC    : UIColor = UIColor(hex: 0xFFCCCC)
+}
+
 extension UIColor {
-    // TODO: memoize these return values at some point
     // These colors are taken from the web styleguide. Any other variations should be
     // double checked with B&F or normalized to one of these..
+
+    // for these colors, just use UIColor.*Color()
     // black - 0x000000
-    class func grey231F20() -> UIColor { return UIColor(hex: 0x231F20) } // HEADING (Profile Header) uses Atlas Grotesk
-    class func grey3() -> UIColor { return UIColor(hex: 0x333333) }
-    class func grey4D() -> UIColor { return UIColor(hex: 0x4D4D4D) }
-    class func grey6() -> UIColor { return UIColor(hex: 0x666666) }
-    class func greyA() -> UIColor { return UIColor(hex: 0xAAAAAA) }
-    class func greyE5() -> UIColor { return UIColor(hex: 0xE5E5E5) }
-    class func greyF1() -> UIColor { return UIColor(hex: 0xF1F1F1) }
     // white - 0xFFFFFF
-    class func yellowFFFFCC() -> UIColor { return UIColor(hex: 0xFFFFCC) } // Used to color @ mention in omnibar/posts
-    class func redFFCCCC() -> UIColor { return UIColor(hex: 0xFFCCCC) } // Used to color @@ direct mention in omnibar/posts (not implemented yet)
     // red - 0xFF0000
+
+    // This color is used as the background on all disabled Ello buttons
+    class func grey231F20() -> UIColor { return ElloColors.grey231F20 }
+
+    // common background color
+    class func grey3() -> UIColor { return ElloColors.grey3 }
+
+    // often used for text:
+    class func greyA() -> UIColor { return ElloColors.greyA }
+
+    // background color for text fields
+    class func greyE5() -> UIColor { return ElloColors.greyE5 }
+
+    // Used to color @ mention in omnibar/posts
+    class func yellowFFFFCC() -> UIColor { return ElloColors.yellowFFFFCC }
+
+    // Used to color @@ direct mention in omnibar/posts (not implemented yet)
+    class func redFFCCCC() -> UIColor { return ElloColors.redFFCCCC }
+
+    // not popular
+    class func grey6() -> UIColor { return ElloColors.grey6 }
+    class func greyF1() -> UIColor { return ElloColors.greyF1 }
+    class func grey4D() -> UIColor { return ElloColors.grey4D }
 }
