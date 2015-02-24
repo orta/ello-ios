9d2ab210e225c6960a2fe261abab863ac570a698
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2015-02-24 11:15:20 -0700

    WIP Adding header link parsing for pagination.

--- Sources/Networking/ElloProvider.swift
+++ Sources/Networking/ElloProvider.swift
@@ -106,24 +106,24 @@ extension MoyaProvider {
     func elloRequest(token: T, method: Moya.Method, parameters: [String: AnyObject], mappingType: MappingType, success: ElloSuccessCompletion, failure: ElloFailureCompletion?) {
         self.request(token, method: method, parameters: parameters, completion: {
             (data, statusCode, response, error) in
-            self.handleRequest(token, method: method, parameters: parameters, data: data, statusCode: statusCode, success: success, failure: failure, isRetry: false, mappingType: mappingType, error: error)
+            self.handleRequest(token, method: method, parameters: parameters, data: data, statusCode: statusCode, success: success, failure: failure, isRetry: false, mappingType: mappingType, error: error, response: response)
         })
     }
 
-    func handleRequest(token: T, method: Moya.Method, parameters: [String: AnyObject], data:NSData?, var statusCode:Int?, success: ElloSuccessCompletion, failure: ElloFailureCompletion?, isRetry: Bool, mappingType: MappingType, error:NSError?) {
+    func handleRequest(token: T, method: Moya.Method, parameters: [String: AnyObject], data:NSData?, var statusCode:Int?, success: ElloSuccessCompletion, failure: ElloFailureCompletion?, isRetry: Bool, mappingType: MappingType, error:NSError?, response: NSURLResponse?) {
         if data != nil && statusCode != nil {
             switch statusCode! {
             case 200...299:
-                self.handleNetworkSuccess(data!, mappingType: mappingType, success: success, failure: failure)
+                self.handleNetworkSuccess(data!, mappingType: mappingType, success: success, failure: failure, response: response)
             case 300...399:
-                self.handleNetworkSuccess(data!, mappingType: mappingType, success: success, failure: failure)
+                self.handleNetworkSuccess(data!, mappingType: mappingType, success: success, failure: failure, response: response)
             case 401:
                 if !isRetry {
                     let authService = AuthService()
                     authService.reAuthenticate({
                         // now retry the previous request that generated the original 401
                         self.request(token, method: method, parameters: parameters, completion: { (data, statusCode, response, error) in
-                            self.handleRequest(token, method: method, parameters: parameters, data: data, statusCode: statusCode, success: success, failure: failure, isRetry: true, mappingType: mappingType, error: error)
+                            self.handleRequest(token, method: method, parameters: parameters, data: data, statusCode: statusCode, success: success, failure: failure, isRetry: true, mappingType: mappingType, error: error, response: response)
                         })
                     },
                     failure: { (_,_) in
@@ -151,7 +151,7 @@ extension MoyaProvider {
         }
     }
 
-    func handleNetworkSuccess(data:NSData, mappingType: MappingType, success:ElloSuccessCompletion, failure:ElloFailureCompletion?) {
+    func handleNetworkSuccess(data:NSData, mappingType: MappingType, success:ElloSuccessCompletion, failure:ElloFailureCompletion?, response: NSURLResponse?) {
         let (mappedJSON: AnyObject?, error) = mapJSON(data)
 
         var mappedObjects: AnyObject?
@@ -164,10 +164,10 @@ extension MoyaProvider {
                 }
 
                 if let node = dict[mappingType.rawValue] as? [[String:AnyObject]] {
-                    mappedObjects = mapToObjectArray(node, fromJSON: mappingType.fromJSON, linked: Store)
+                    mappedObjects = mapToObjectArray(node, fromJSON: mappingType.fromJSON, response: response)
                 }
                 else if let node = dict[mappingType.rawValue] as? [String:AnyObject] {
-                    mappedObjects = mapToObject(node, fromJSON: mappingType.fromJSON, linked: Store)
+                    mappedObjects = mapToObject(node, fromJSON: mappingType.fromJSON, response: response)
                 }
             }
 
@@ -231,7 +231,7 @@ extension MoyaProvider {
 
             if mappedJSON != nil && error == nil {
                 if let node = mappedJSON?[MappingType.ErrorsType.rawValue] as? [String:AnyObject] {
-                    elloNetworkError = mapToObject(node, fromJSON: MappingType.ErrorType.fromJSON, linked: Store) as? ElloNetworkError
+                    elloNetworkError = mapToObject(node, fromJSON: MappingType.ErrorType.fromJSON, response: nil) as? ElloNetworkError
                 }
             }
         }
@@ -259,19 +259,25 @@ extension MoyaProvider {
         return (json, error)
     }
 
-    func mapToObjectArray(object: AnyObject?, fromJSON: FromJSONClosure, linked:ElloLinkedStore) -> [JSONAble]? {
+    func mapToObjectArray(object: AnyObject?, fromJSON: FromJSONClosure, response: NSURLResponse?) -> [JSONAble]? {
 
         if let dicts = object as? [[String:AnyObject]] {
-            let jsonables:[JSONAble] =  dicts.map({ return fromJSON(data: $0) })
+            let jsonables:[JSONAble] =  dicts.map {
+                let jsonable = fromJSON(data: $0)
+                jsonable.parseResponse(response)
+                return jsonable
+            }
             return jsonables
         }
 
         return nil
     }
 
-    func mapToObject(object:AnyObject?, fromJSON: FromJSONClosure, linked:ElloLinkedStore) -> JSONAble? {
+    func mapToObject(object:AnyObject?, fromJSON: FromJSONClosure, response: NSURLResponse?) -> JSONAble? {
         if let dict = object as? [String:AnyObject] {
-            return fromJSON(data: dict)
+            let jsonable = fromJSON(data: dict)
+            jsonable.parseResponse(response)
+            return jsonable
         }
         else {
             return nil
