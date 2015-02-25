a7e69781aeeb926ae80c4882055d46fad1bd9978
Sean Dougherty <seancdougherty@gmail.com>
2015-02-25 14:46:53 -0700

    fixed merge conflicts

--- Sources/Networking/ElloProvider.swift
+++ Sources/Networking/ElloProvider.swift
@@ -30,7 +30,7 @@ struct ElloProvider {
         case StatusUnknown = 1_000_000
 
         var defaultData: NSData {
-            return stubbedResponse(String(self.rawValue))
+            return stubbedData(String(self.rawValue))
         }
 
         var notificationName: NSString {
@@ -60,7 +60,7 @@ struct ElloProvider {
     }
 
     static var endpointsClosure = { (target: ElloAPI, method: Moya.Method, parameters: [String: AnyObject]) -> Endpoint<ElloAPI> in
-        var endpoint = Endpoint<ElloAPI>(URL: url(target), sampleResponse: .Success(200, target.sampleData), method: method, parameters: parameters)
+        var endpoint = Endpoint<ElloAPI>(URL: url(target), sampleResponse: .Lazy({return .Success(200, target.sampleData, target.sampleResponse)}), method: method, parameters: parameters)
 
         switch target {
         case .Auth, .ReAuth:
