bf870b6c811565f1cefb737cb15ce70c69b57a14
Tony DiPasquale <tony@thoughtbot.com>
2015-03-05 11:34:56 -0500

    Add SHA1 + salt hashing

--- Sources/Ello-Bridging-Header.h
+++ Sources/Ello-Bridging-Header.h
@@ -4,4 +4,5 @@
 #import "JTSImageInfo.h"
 #import "UITabBarController+NBUAdditions.h"
 #import "KINWebBrowserViewController.h"
+#import <CommonCrypto/CommonCrypto.h>
 //#import <Pods/ElloKeys.h>
--- Sources/Extensions/StringExtensions.swift
+++ Sources/Extensions/StringExtensions.swift
@@ -8,9 +8,26 @@
 
 import Foundation
 
+private let salt = "***REMOVED***"
+
 extension String {
 
     func stripHTML() -> String {
         return self.stringByReplacingOccurrencesOfString("<[^>]+>", withString: "", options: NSStringCompareOptions.RegularExpressionSearch, range: nil)
+    }
+
+    var SHA1String: String? {
+        if let data = (salt + self).dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false) {
+            
+            var digest = [UInt8](count: Int(CC_SHA1_DIGEST_LENGTH), repeatedValue: 0)
+            CC_SHA1(data.bytes, CC_LONG(data.length), &digest)
+            let output = NSMutableString(capacity: Int(CC_SHA512_DIGEST_LENGTH));
+            for byte in digest {
+                output.appendFormat("%02x", byte);
+            }
+            
+            return output
+        }
+        return .None
     }
 }
