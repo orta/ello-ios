3599b266641b23d24dd5602d07a90127e42f3444
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2015-03-06 10:32:44 -0700

    Fixes post loading for users.
    [Fixes #89808310]

--- Sources/Model/ElloLinkedStore.swift
+++ Sources/Model/ElloLinkedStore.swift
@@ -45,36 +45,40 @@ struct ElloLinkedStore {
                 }
             }
             else if let strArray = links[key] as? [String] {
-                if let mappingType = MappingType(rawValue: key) {
-                    if mappingType.isOrdered {
-                        var linkArray = [JSONAble]()
-                        for str in strArray {
-                            if let linkJSON = Store.store[key]?[str] as? [String: AnyObject] {
-                                let linkModel = mappingType.fromJSON(data: linkJSON)
-//                                let user = model as? User
-//                                let post = linkModel  as? Post
-//                                if user != nil && post != nil {
-//                                    post!.author = user!
-//                                }
-                                linkArray.append(linkModel)
-                            }
-                        }
-                        modelLinks[key] = linkArray
-                    }
-                    else {
-                        var linkDict = [String: JSONAble]()
-                        for link: String in strArray {
-                            if let linkJSON = Store.store[key]?[link] as? [String: AnyObject] {
-                                let linkModel = mappingType.fromJSON(data: linkJSON)
-                                linkDict[link] = linkModel
-                            }
-                        }
-                        modelLinks[key] = linkDict
-                    }
-                }
+                modelLinks = parseArray(key, strArray: strArray, modelLinks: modelLinks)
+            }
+            else if let strArray = links[key]?["ids"] as? [String] {
+                modelLinks = parseArray(key, strArray: strArray, modelLinks: modelLinks)
             }
         }
         return modelLinks
+    }
+
+    static func parseArray(key: String, strArray: [String], modelLinks: [String: AnyObject]) -> [String: AnyObject] {
+        var modelLinksCopy = modelLinks
+        if let mappingType = MappingType(rawValue: key) {
+            if mappingType.isOrdered {
+                var linkArray = [JSONAble]()
+                for str in strArray {
+                    if let linkJSON = Store.store[key]?[str] as? [String: AnyObject] {
+                        let linkModel = mappingType.fromJSON(data: linkJSON)
+                        linkArray.append(linkModel)
+                    }
+                }
+                modelLinksCopy[key] = linkArray
+            }
+            else {
+                var linkDict = [String: JSONAble]()
+                for link: String in strArray {
+                    if let linkJSON = Store.store[key]?[link] as? [String: AnyObject] {
+                        let linkModel = mappingType.fromJSON(data: linkJSON)
+                        linkDict[link] = linkModel
+                    }
+                }
+                modelLinksCopy[key] = linkDict
+            }
+        }
+        return modelLinksCopy
     }
 
 }
