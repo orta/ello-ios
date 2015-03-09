94c619e95ad8c3d0bf39d2ad9f7728b9e4e12973
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2015-03-09 16:37:24 -0600

    WIP: starting to try to get pull requests in range

--- bin/generate_release_notes.rb
+++ bin/generate_release_notes.rb
@@ -21,74 +21,77 @@ config = YAML::load_file('bin/release-notes-config.yml')
 
 # read the previous commit
 last_commit = config["previous-commit"]
-
 # open the repo
 repo = Rugged::Repository.new('.git')
-
-# read the latest commit from head
-latest_commit = repo.head.target.oid
-
-# Parse out story IDs from the current commit
-commit_range = "#{last_commit}..#{latest_commit}"
-story_ids = `git log --format=%B #{commit_range}`.scan(/\[\#(\d+)\]/).map(&:first)
-
-PivotalTracker::Client.token = "fc826e4f5dd2622f519e09c62f32b982"
-PivotalTracker::Client.use_ssl = true
-project = PivotalTracker::Project.find("1214202")
-stories = story_ids.uniq.map { |story_id| project.stories.find(story_id) }.compact
-
-build_number = what_version
-marketing_version = what_marketing_version
-testflight_message = "Ello #{marketing_version} Build #{build_number}"
-
-# Append story notes
-if stories.size > 0
-testflight_message << <<-EOF
-
-Tracker stories:
-#{stories.map { |s| "[#{s.id}] #{s.name}" } * "\n"}
-
-EOF
+repo.branches.each do |branch|
+  print "\r\rBRANCH\r"
+  print branch
 end
 
-puts testflight_message
+# # read the latest commit from head
+# latest_commit = repo.head.target.oid
 
-release_note_message = "###Ello #{marketing_version} Build #{build_number}"
+# # Parse out story IDs from the current commit
+# commit_range = "#{last_commit}..#{latest_commit}"
+# story_ids = `git log --format=%B #{commit_range}`.scan(/\[\#(\d+)\]/).map(&:first)
 
-# Append story notes
-if stories.size > 0
-release_note_message << <<-EOF
+# PivotalTracker::Client.token = "fc826e4f5dd2622f519e09c62f32b982"
+# PivotalTracker::Client.use_ssl = true
+# project = PivotalTracker::Project.find("1214202")
+# stories = story_ids.uniq.map { |story_id| project.stories.find(story_id) }.compact
 
-####Tracker stories:
-#{stories.map { |s| "* [#{s.id}](#{s.url}) #{s.name}" } * "\n"}
+# build_number = what_version
+# marketing_version = what_marketing_version
+# testflight_message = "Ello #{marketing_version} Build #{build_number}"
 
-EOF
-end
+# # Append story notes
+# if stories.size > 0
+# testflight_message << <<-EOF
 
-crashlytics_release_notes = release_note_message.dup
+# Tracker stories:
+# #{stories.map { |s| "[#{s.id}] #{s.name}" } * "\n"}
 
-commit_notes = `git log --format=%s #{commit_range}`
-formatted_commit_notes = %(#{commit_notes}).split(/\n/).map { |s| "* #{s}" } * "\n"
+# EOF
+# end
 
-# Append commit notes
-release_note_message << <<-EOF
-####Commit notes:
+# puts testflight_message
 
-#{formatted_commit_notes}
+# release_note_message = "###Ello #{marketing_version} Build #{build_number}"
 
-EOF
+# # Append story notes
+# if stories.size > 0
+# release_note_message << <<-EOF
 
-open('release-notes.md', 'a') { |f|
- f.puts "\n-----------------"
- f.puts "#{release_note_message}"
- f.puts "-----------------\n"
-}
+# ####Tracker stories:
+# #{stories.map { |s| "* [#{s.id}](#{s.url}) #{s.name}" } * "\n"}
 
-if ARGV[0] && ARGV[0].split(',').include?("testers")
-  open('crashlytics-release-notes.md', 'w') { |f|
-   f.puts crashlytics_release_notes
-  }
+# EOF
+# end
 
-  config["previous-commit"] = "#{latest_commit}"
-  File.open('bin/release-notes-config.yml', 'w') {|f| f.write config.to_yaml }
-end
+# crashlytics_release_notes = release_note_message.dup
+
+# commit_notes = `git log --format=%s #{commit_range}`
+# formatted_commit_notes = %(#{commit_notes}).split(/\n/).map { |s| "* #{s}" } * "\n"
+
+# # Append commit notes
+# release_note_message << <<-EOF
+# ####Commit notes:
+
+# #{formatted_commit_notes}
+
+# EOF
+
+# open('release-notes.md', 'a') { |f|
+#  f.puts "\n-----------------"
+#  f.puts "#{release_note_message}"
+#  f.puts "-----------------\n"
+# }
+
+# if ARGV[0] && ARGV[0].split(',').include?("testers")
+#   open('crashlytics-release-notes.md', 'w') { |f|
+#    f.puts crashlytics_release_notes
+#   }
+
+#   config["previous-commit"] = "#{latest_commit}"
+#   File.open('bin/release-notes-config.yml', 'w') {|f| f.write config.to_yaml }
+# end
