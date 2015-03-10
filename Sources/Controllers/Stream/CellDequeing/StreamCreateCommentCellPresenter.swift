3c440d638c75aef3500af2533f413e74f66bd9b9
Colin T.A. Gray <colinta@gmail.com>
2015-03-10 14:14:44 -0600

    added StreamCreateCommentCell, StreamCreateCommentCellPresenter files

---
+++ Sources/Controllers/Stream/CellDequeing/StreamCreateCommentCellPresenter.swift
@@ -0,0 +1,21 @@
+//
+//  StreamCreateCommentCellPresenter.swift
+//  Ello
+//
+//  Created by Colin Gray on 3/10/2015.
+//  Copyright (c) 2015 Ello. All rights reserved.
+//
+
+struct StreamCreateCommentCellPresenter {
+
+    static func configure(
+        cell:UICollectionViewCell,
+        streamCellItem:StreamCellItem,
+        streamKind: StreamKind,
+        indexPath: NSIndexPath)
+    {
+        if let cell = cell as? StreamCreateCommentCell {
+        }
+    }
+
+}
---
+++ Sources/Controllers/Stream/Cells/StreamCreateCommentCell.swift
@@ -0,0 +1,10 @@
+//
+//  StreamCreateCommentCell.swift
+//  Ello
+//
+//  Created by Colin Gray on 3/10/2015.
+//  Copyright (c) 2015 Ello. All rights reserved.
+//
+
+class StreamCreateCommentCell : UICollectionViewCell {
+}
