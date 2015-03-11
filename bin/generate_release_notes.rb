1bcc1fdcff2c1e60a9bc403205231d5519ce94ca
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2015-03-11 11:23:02 -0600

    Update release notes to be a class.
    * Pulls from git for version and build number
    * Will loop back until it finds the previous commit stored
    
    [Finishes #89935658]

--- bin/generate_release_notes.rb
+++ bin/generate_release_notes.rb
@@ -8,62 +8,77 @@ require 'yaml'
 # load .env vars
 Dotenv.load
 
-# grab out build verion info
-def build_version
- output = `agvtool what-version -terse`
- output.length > 0 ? output : nil
-end
+class GenerateReleaseNotes
 
-def marketing_version
- output = `agvtool what-marketing-version -terse`
- output.scan(/\=(.+)$/).flatten.first
-end
+  def initialize(repo_name, previous_sha_file, access_token)
+    @repo_name = repo_name
+    @pull_request_notes = []
+    # Grab out previous sha
+    @previous_sha_file = previous_sha_file
+    @previous_sha_yaml = YAML::load_file(@previous_sha_file)
+    set_versions
+    # create github api client and grab initial commits
+    @client = Octokit::Client.new(access_token: access_token)
+    commits = @client.commits(repo_name, 'master')
+    @newest_sha = commits.first[:sha]
+    # start creating the notes
+    scan_commits commits
+    # update the notes
+    update_release_notes
+  end
 
-current_notes = ""
+  # grab out build verion info
+  def set_versions
+    @git_release_version = `git describe --tags --always --abbrev=0`
+    @number_of_commits = `git rev-list master | wc -l | tr -d ' '`
+  end
 
-# Grab out pull request info within range
-repo_name = 'ello/ello-ios'
-previous_sha = YAML::load_file('bin/previous-sha.yml')
-client = Octokit::Client.new(access_token: ENV['GITHUB_API_TOKEN'])
-# These are grabbed from newest to oldest by 30
-# TODO: figure out how to get more..
-commits = client.commits(repo_name, 'master')
-commits.each do |commit|
-  break if previous_sha["previous-sha"] == commit[:sha]
-  match = commit[:commit][:message].match(/pull request #(\d+) from/)
-  if match
-    pr_num = match.captures[0]
-    pr = client.pull_request(repo_name, pr_num)
-    if pr[:state] == 'closed'
-      current_notes << "##{pr_num} - #{pr[:title]}"
-      current_notes << pr[:body]
-      current_notes << "\n\n---\n"
+  # add PRs from commits
+  def scan_commits(commits)
+    commits.each do |commit|
+      return true if @previous_sha_yaml['previous-sha'] == commit[:sha]
+      match = commit[:commit][:message].match(/pull request #(\d+) from/)
+      if match
+        pr_num = match.captures[0]
+        pr = @client.pull_request @repo_name, pr_num
+        if pr[:state] == 'closed'
+          @pull_request_notes << "#### ##{pr_num} - #{pr[:title]}\n#{pr[:body]}"
+        end
+      end
+    end
+    scan_commits @client.commits_before(@repo_name, DateTime.parse(commits.last[:commit][:author][:date].to_s))
+  end
+
+  def update_release_notes
+    # new release notes
+    release_notes = "### Ello #{@git_release_version} Build #{@number_of_commits} #{Time.now.strftime("%B %-d, %Y")}\n\n"
+    release_notes << <<-EOF
+    #{@pull_request_notes.count > 0 ? @pull_request_notes.join("\n\n------\n\n") : 'No completed pull requests since last distribution.'}
+    #{"\n------------\n"}
+    EOF
+
+    if ARGV[0] && ARGV[0].split(',').include?("testers")
+      # prepend new contents into release-notes
+      old = File.open('release-notes.md', 'a')
+      new = File.open('release-notes.new.md', 'w')
+      File.open(new, 'w') { |f|
+        f.puts release_notes
+        f.puts File.read(old)
+      }
+      File.rename(new, old)
+
+      # add release_notes to crashlytics-release-notes
+      File.open('Build/crashlytics-release-notes.md', 'w') { |f| f.write release_notes }
+
+      # update the latest commit from here
+      @previous_sha_yaml["previous-sha"] = @newest_sha
+      File.open(@previous_sha_file, 'w') {|f| f.write @previous_sha_yaml.to_yaml }
+    else
+      puts release_notes
     end
   end
+
 end
 
-# new release notes
-release_notes = "### Ello #{marketing_version} Build #{build_version} #{Time.now.strftime("%B %-d, %Y")}\n\n"
-release_notes << <<-EOF
-#{current_notes.length ? current_notes : 'No completed pull requests since last distribution.'}
-#{"\n-----------------\n"}
-EOF
-
-# if ARGV[0] && ARGV[0].split(',').include?("testers")
-  # prepend new contents into release-notes
-  old = File.open('release-notes.md', 'a')
-  new = File.open('release-notes.new.md', 'w')
-  File.open(new, 'w') { |f|
-    f.write release_notes
-    f.puts File.read(old)
-  }
-  File.rename(new, old)
-
-  # add release_notes to crashlytics-release-notes
-  File.open('Build/crashlytics-release-notes.md', 'w') { |f| f.write release_notes }
-
-  # update the latest commit from here
-  previous_sha["previous-sha"] = "#{commits.first[:sha]}"
-  File.open('bin/previous-sha.yml', 'w') {|f| f.write previous_sha.to_yaml }
-# end
+GenerateReleaseNotes.new('ello/ello-ios', 'bin/previous-sha.yml', ENV['GITHUB_API_TOKEN'])
 
