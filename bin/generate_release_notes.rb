9b3cf73739f4b5baf205d5ef4a74dd14e73210cb
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2015-03-11 11:48:24 -0600

    Change formatting of the build number.

--- bin/generate_release_notes.rb
+++ bin/generate_release_notes.rb
@@ -51,7 +51,7 @@ class GenerateReleaseNotes
 
   def update_release_notes
     # new release notes
-    release_notes = "### Ello #{@git_release_version} Build #{@number_of_commits} #{Time.now.strftime("%B %-d, %Y")}\n\n"
+    release_notes = "### Ello Build #{@number_of_commits}(#{@git_release_version}) #{Time.now.strftime("%B %-d, %Y")}\n\n"
     release_notes << <<-EOF
     #{@pull_request_notes.count > 0 ? @pull_request_notes.join("\n\n------\n\n") : 'No completed pull requests since last distribution.'}
     #{"\n------------\n"}
