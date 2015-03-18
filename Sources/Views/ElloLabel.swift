f33a5b560df2e659278931f40aa93a92c5a3c7bf
Sean Dougherty <seancdougherty@gmail.com>
2015-03-18 13:32:21 -0600

    Add Ello Label

---
+++ Sources/Views/ElloLabel.swift
@@ -0,0 +1,56 @@
+//
+//  ElloLabel.swift
+//  Ello
+//
+//  Created by Sean on 3/18/15.
+//  Copyright (c) 2015 Ello. All rights reserved.
+//
+
+import Foundation
+
+
+class ElloLabel: UILabel {
+
+    func labelTextColor() -> UIColor {
+        return UIColor.whiteColor()
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        if let text = self.text {
+            self.setLabelText(text)
+        }
+    }
+
+    func attributes(title:String) -> [NSObject : AnyObject] {
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 10
+
+        var attributedString = NSMutableAttributedString(string: title)
+        var range = NSRange(location: 0, length: countElements(title))
+        return [
+            NSFontAttributeName : UIFont.typewriterFont(12.0),
+            NSForegroundColorAttributeName : self.labelTextColor(),
+            NSParagraphStyleAttributeName : paragraphStyle
+        ]
+    }
+
+    func height() -> CGFloat {
+        if let text = self.text {
+            let nstext:NSString = NSString(string: text)
+            return nstext.boundingRectWithSize(CGSize(width: self.frame.size.width, height: CGFloat.max),
+                options: .UsesLineFragmentOrigin,
+                attributes: attributes(text),
+                context: nil).size.height
+        }
+        return 0.0
+    }
+
+    func setLabelText(title:String) {
+        var attributedString = NSMutableAttributedString(string: title)
+        var range = NSRange(location: 0, length: countElements(title))
+        attributedString.addAttributes(attributes(title), range: range)
+        self.attributedText = attributedString
+    }
+    
+}
