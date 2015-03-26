0ac89423c465b41c3bfee8b13e1dc619ab1e23c4
Tony DiPasquale <tony@thoughtbot.com>
2015-03-26 17:13:46 -0400

    Add instance nib loading function

--- Sources/Extensions/NSObjectClassName.swift
+++ Sources/Extensions/NSObjectClassName.swift
@@ -2,6 +2,6 @@ extension NSObject {
     class func readableClassName() -> String {
         let classString = NSStringFromClass(self)
         let range = classString.rangeOfString(".", options: NSStringCompareOptions.CaseInsensitiveSearch, range: Range<String.Index>(start:classString.startIndex, end: classString.endIndex), locale: nil)
-        return classString.substringFromIndex(range!.endIndex)
+        return range.map { classString.substringFromIndex($0.endIndex) } ?? classString
     }
 }
--- Sources/Extensions/UIViewNibLoading.swift
+++ Sources/Extensions/UIViewNibLoading.swift
@@ -4,4 +4,10 @@ extension UIView {
         let vs = nib.instantiateWithOwner(.None, options: .None)
         return vs[0] as! T
     }
+
+    func loadFromNib<T: UIView>() -> T {
+        let nib = UINib(nibName: self.dynamicType.readableClassName(), bundle: NSBundle(forClass: self.dynamicType))
+        let vs = nib.instantiateWithOwner(self, options: .None)
+        return vs[0] as T
+    }
 }
