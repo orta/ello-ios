521c608f3d9c9ad7a9ace1b0060b8f974432c8e3
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2015-04-02 13:27:47 -0600

    Whitespace updates for extension.

--- Sources/Extensions/UICollectionViewExtensions.swift
+++ Sources/Extensions/UICollectionViewExtensions.swift
@@ -9,9 +9,7 @@
 import Foundation
 
 extension UICollectionView {
-
     public func lastIndexPathForSection(section: Int) -> NSIndexPath {
         return NSIndexPath(forItem: self.numberOfItemsInSection(section) - 1, inSection: section)
     }
-
 }
