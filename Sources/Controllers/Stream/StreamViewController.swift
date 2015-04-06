b2d9fc91eb94f2bcacd3c408ab1d68e2577aeb6a
Colin T.A. Gray <colinta@gmail.com>
2015-04-06 17:12:38 -0600

    yes, I actually *reverted* to swift 1.1 syntax here
    these multiline lets are not readable.  say what you will about
    nested ifs, at least they're easy to visually parse!

--- Sources/Controllers/Stream/StreamViewController.swift
+++ Sources/Controllers/Stream/StreamViewController.swift
@@ -200,11 +200,14 @@ public class StreamViewController: BaseElloViewController {
         }
 
         let commentItem = commentItems[0]
-        if let comment = commentItem.jsonable as? Comment,
-           let parentPost = comment.parentPost,
-           let indexPath = dataSource.createCommentIndexPathForPost(parentPost) {
-            let newCommentIndexPath = NSIndexPath(forRow: indexPath.row + 1, inSection: indexPath.section)
-            self.insertUnsizedCellItems(commentItems, startingIndexPath: newCommentIndexPath)
+        if let comment = commentItem.jsonable as? Comment {
+            if let parentPost = comment.parentPost {
+                if let indexPath = dataSource.createCommentIndexPathForPost(parentPost) {
+                    // insert the items below the create comment button
+                    let newCommentIndexPath = NSIndexPath(forRow: indexPath.row + 1, inSection: indexPath.section)
+                    self.insertUnsizedCellItems(commentItems, startingIndexPath: newCommentIndexPath)
+                }
+            }
         }
     }
 
