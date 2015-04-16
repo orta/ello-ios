4577c51b3f2dd0b72b216530486d4e81d1040b99
Gordon Fontenot <gordon@thoughtbot.com>
2015-04-16 14:00:30 -0400

    Add Tracker class
    This has the mockup interface for how the Tracker will work

---
+++ Sources/Model/Tracker.swift
@@ -0,0 +1,72 @@
+//
+//  Tracker.swift
+//  Ello
+//
+//  Created by Gordon Fontenot on 4/9/15.
+//  Copyright (c) 2015 Ello. All rights reserved.
+//
+
+private let AnalyticsAPIKey = ""
+
+public struct Tracker {
+    public static let sharedTracker = Tracker()
+
+    public init() {
+        // authenticate with Segment.io
+        // let configuration = SEGAnalyticsConfiguration.configurationWithKey(AnalyticsAPIKey)
+        // SEGAnalytics.setupWithConfiguration(configuration)
+    }
+}
+
+public extension Tracker {
+    func identify(user: User) {
+        // SEGAnalytics.sharedAnalytics().identify(user.userId,
+        //                                          traits: {
+        //                                              "name": user.name,
+        //                                              "email": user.email ?? "None Provided"
+        //                                          })
+    }
+}
+
+public extension Tracker {
+    func sessionStarted() { }
+    func sessionEnded() { }
+
+    func screenAppeared(name: String) { }
+
+    func contentCreated(content: String)() { }
+    func contentCreationCanceled(content: String)() { }
+    func contentCreationFailed() { }
+
+    func contentFlagged(content: String, flag: ContentFlagger.AlertOption) { }
+    func contentFlaggingCanceled(content: String) { }
+
+    func postBarVisibilityChanged(visible: Bool) { }
+
+    func viewedImage() { }
+
+    func postReposted() { }
+    func postShared() { }
+
+    func inlineCommentsViewed() { }
+
+    func userBlocked() { }
+    func userMuted() { }
+    func userBlockCanceled() { }
+
+    func imageAddedFromCamera() { }
+    func imageAddedFromLibrary() { }
+    func addImageCanceled() { }
+
+    func friendAdded(fromFindScreen: Bool = false) { }
+    func userInvited() { }
+
+    func pushNotificationPreferenceChanged(enabled: Bool) { }
+    func contactAccessPreferenceChanged(enabled: Bool) { }
+
+    func drawerClosed() { }
+
+    func viewsButtonTapped() { }
+
+    func encounteredNetworkError(error: NSError) { }
+}
