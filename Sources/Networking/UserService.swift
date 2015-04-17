e637df0c63f3a507059a635b215a318f62e8064d
Colin T.A. Gray <colinta@gmail.com>
2015-04-17 16:41:55 -0600

    added 'UserService', with 'join()' methods
    For joining/signing up.  Invitation Code is optional,
    so there's a helper method that excludes it.  also fixes
    some methods in ElloAPI referencing Join

---
+++ Sources/Networking/UserService.swift
@@ -0,0 +1,51 @@
+//
+//  UserService.swift
+//  Ello
+//
+//  Created by Sean on 4/6/15.
+//  Copyright (c) 2015 Ello. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+import Moya
+import SwiftyJSON
+
+
+public struct UserService {
+
+    public init(){}
+
+    public func join(
+        #email: String,
+        username: String,
+        password: String,
+        success: UserSuccessCompletion,
+        failure: ElloFailureCompletion?)
+    {
+        return join(email: email, username: username, password: password, invitationCode: nil, success: success, failure: failure)
+    }
+
+    public func join(
+        #email: String,
+        username: String,
+        password: String,
+        invitationCode: String?,
+        success: UserSuccessCompletion,
+        failure: ElloFailureCompletion?)
+    {
+        ElloProvider.elloRequest(ElloAPI.Join(email: email, username: username, password: password, invitationCode: invitationCode),
+            method: .POST,
+            success: { (data, responseConfig) in
+                if let user = data as? User {
+                    success(user: user, responseConfig: responseConfig)
+                }
+                else {
+                    ElloProvider.unCastableJSONAble(failure)
+                }
+            },
+            failure: failure
+        )
+    }
+ }
