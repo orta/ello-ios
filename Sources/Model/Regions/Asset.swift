71e93eb03270bf144be26bc6fdb16155893395af
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2015-04-22 12:28:07 -0600

    Converts user avatar and cover to use `Asset`
    * Adds large, regular, and small for assets
    * Adds static `parseAsset` class method to `Asset` for passing in a specific node

--- Sources/Model/Regions/Asset.swift
+++ Sources/Model/Regions/Asset.swift
@@ -25,6 +25,10 @@ public final class Asset: JSONAble {
     public var xhdpi: Attachment?
     public var xxhdpi: Attachment?
     public var original: Attachment?
+    // optional avatar
+    public var large: Attachment?
+    public var regular: Attachment?
+    public var small: Attachment?
     // computed
     var isGif: Bool {
         return self.optimized?.type == "image/gif"
@@ -50,7 +54,9 @@ public final class Asset: JSONAble {
 
     public required init(coder aDecoder: NSCoder) {
         let decoder = Decoder(aDecoder)
+        // required
         self.id = decoder.decodeKey("id")
+        // optional
         self.optimized = decoder.decodeOptionalKey("optimized")
         self.smallScreen = decoder.decodeOptionalKey("smallScreen")
         self.ldpi = decoder.decodeOptionalKey("ldpi")
@@ -59,12 +65,17 @@ public final class Asset: JSONAble {
         self.xhdpi = decoder.decodeOptionalKey("xhdpi")
         self.xxhdpi = decoder.decodeOptionalKey("xxhdpi")
         self.original = decoder.decodeOptionalKey("original")
+        // optional avatar
+        self.large = decoder.decodeOptionalKey("large")
+        self.regular = decoder.decodeOptionalKey("regular")
+        self.small = decoder.decodeOptionalKey("small")
         super.init(coder: aDecoder)
     }
 
     public override func encodeWithCoder(encoder: NSCoder) {
-
-        encoder.encodeObject(self.id, forKey: "id")
+        // required
+        encoder.encodeObject(id, forKey: "id")
+        // optional
         encoder.encodeObject(optimized, forKey: "optimized")
         encoder.encodeObject(smallScreen, forKey: "smallScreen")
         encoder.encodeObject(ldpi, forKey: "ldpi")
@@ -73,6 +84,10 @@ public final class Asset: JSONAble {
         encoder.encodeObject(xhdpi, forKey: "xhdpi")
         encoder.encodeObject(xxhdpi, forKey: "xxhdpi")
         encoder.encodeObject(original, forKey: "original")
+        // optional avatar
+        encoder.encodeObject(large, forKey: "large")
+        encoder.encodeObject(regular, forKey: "regular")
+        encoder.encodeObject(small, forKey: "small")
         super.encodeWithCoder(encoder)
     }
     
@@ -80,34 +95,46 @@ public final class Asset: JSONAble {
 
     override class public func fromJSON(data:[String: AnyObject], fromLinked: Bool = false) -> JSONAble {
         let json = JSON(data)
-        let attachment = data["attachment"] as? [String:AnyObject]
-        var asset = Asset(id: json["id"].stringValue)
+        return parseAsset(json["id"].stringValue, node: data["attachment"] as? [String: AnyObject])
+    }
+
+    class public func parseAsset(id: String, node: [String: AnyObject]?) -> Asset {
+        var asset = Asset(id: id)
         // optional
-        if let optimized = attachment?["optimized"] as? [String: AnyObject] {
+        if let optimized = node?["optimized"] as? [String: AnyObject] {
             asset.optimized = Attachment.fromJSON(optimized) as? Attachment
         }
-        if let smallScreen = attachment?["small_screen"] as? [String: AnyObject] {
+        if let smallScreen = node?["small_screen"] as? [String: AnyObject] {
             asset.smallScreen = Attachment.fromJSON(smallScreen) as? Attachment
         }
-        if let ldpi = attachment?["ldpi"] as? [String: AnyObject] {
+        if let ldpi = node?["ldpi"] as? [String: AnyObject] {
             asset.ldpi = Attachment.fromJSON(ldpi) as? Attachment
         }
-        if let mdpi = attachment?["mdpi"] as? [String: AnyObject] {
+        if let mdpi = node?["mdpi"] as? [String: AnyObject] {
             asset.mdpi = Attachment.fromJSON(mdpi) as? Attachment
         }
-        if let hdpi = attachment?["hdpi"] as? [String: AnyObject] {
+        if let hdpi = node?["hdpi"] as? [String: AnyObject] {
             asset.hdpi = Attachment.fromJSON(hdpi) as? Attachment
         }
-        if let xhdpi = attachment?["xhdpi"] as? [String: AnyObject] {
+        if let xhdpi = node?["xhdpi"] as? [String: AnyObject] {
             asset.xhdpi = Attachment.fromJSON(xhdpi) as? Attachment
         }
-        if let xxhdpi = attachment?["xxhdpi"] as? [String: AnyObject] {
+        if let xxhdpi = node?["xxhdpi"] as? [String: AnyObject] {
             asset.xxhdpi = Attachment.fromJSON(xxhdpi) as? Attachment
         }
-        if let original = attachment?["original"] as? [String: AnyObject] {
+        if let original = node?["original"] as? [String: AnyObject] {
             asset.original = Attachment.fromJSON(original) as? Attachment
         }
-
+        // optional avatar
+        if let large = node?["large"] as? [String: AnyObject] {
+            asset.large = Attachment.fromJSON(large) as? Attachment
+        }
+        if let regular = node?["regular"] as? [String: AnyObject] {
+            asset.regular = Attachment.fromJSON(regular) as? Attachment
+        }
+        if let small = node?["small"] as? [String: AnyObject] {
+            asset.small = Attachment.fromJSON(small) as? Attachment
+        }
         return asset
     }
 }
