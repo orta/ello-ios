589cfe421c8b36c4f9d5ec7becf35f77baf237df
Colin T.A. Gray <colinta@gmail.com>
2015-04-27 17:08:16 -0600

    use a variable name here... though...
    I'm not sure this code is right.  Why add 300 to the offset?

--- Sources/Controllers/Stream/StreamViewController.swift
+++ Sources/Controllers/Stream/StreamViewController.swift
@@ -479,15 +479,18 @@ extension StreamViewController : UIScrollViewDelegate {
     }
 
     private func loadNextPage(scrollView: UIScrollView) {
-        if scrollView.contentOffset.y + self.view.frame.height + 300 > scrollView.contentSize.height {
+        let scrollPastThreshold = CGFloat(300)
+        if scrollView.contentOffset.y + self.view.frame.height + scrollPastThreshold > scrollView.contentSize.height {
             if allOlderPagesLoaded == true { return }
             if responseConfig?.totalPagesRemaining == "0" { return }
+
             if let nextQueryItems = responseConfig?.nextQueryItems {
                 if dataSource.visibleCellItems.count > 0 {
                     let lastCellItem: StreamCellItem = dataSource.visibleCellItems[dataSource.visibleCellItems.count - 1]
                     if lastCellItem.type == .StreamLoading { return }
                     appendStreamCellItems([StreamLoadingCell.streamCellItem()])
                 }
+
                 let scrollAPI = ElloAPI.InfiniteScroll(queryItems: nextQueryItems) { return self.streamKind.endpoint }
                 streamService.loadStream(scrollAPI,
                     streamKind: streamKind,
