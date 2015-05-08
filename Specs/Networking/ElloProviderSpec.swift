0c84c09b764a5e384155f0bebb88c8a8d5fe55db
Sean Dougherty <seancdougherty@gmail.com>
2015-05-08 16:42:09 -0600

    Colin, fix this! :)

--- Specs/Networking/ElloProviderSpec.swift
+++ Specs/Networking/ElloProviderSpec.swift
@@ -61,7 +61,7 @@ class ElloProviderSpec: QuickSpec {
                         ElloProvider.sharedProvider = ElloProvider.DefaultProvider()
                     }
 
-                    context("401") {
+                    xcontext("401") {
 
                         it("posts a notification with a status of 401") {
 
