2d6c0fa45a5442e512c250a8b3066f8077caf315
Sean Dougherty <seancdougherty@gmail.com>
2015-05-12 10:39:23 -0600

    Add static function to UITabBarItem for retrieving an SVGImage item

---
+++ Sources/Extensions/UITabBarItem.swift
@@ -0,0 +1,18 @@
+//
+//  UITabBarItem.swift
+//  Ello
+//
+//  Created by Sean on 5/11/15.
+//  Copyright (c) 2015 Ello. All rights reserved.
+//
+
+import UIKit
+import SVGKit
+
+extension UITabBarItem {
+    public static func svgItem(imageName: String) -> UITabBarItem {
+        let iconImage = SVGKImage(named: "\(imageName)_normal.svg").UIImage!
+        let iconSelectedImage = SVGKImage(named: "\(imageName)_selected.svg").UIImage!
+        return UITabBarItem(title: nil, image: iconImage, selectedImage: iconSelectedImage)
+    }
+}
