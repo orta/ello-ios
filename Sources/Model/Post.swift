28c3e5fb4b3d93e4f5e94a90ea96c40ab05f1b6e
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2015-05-14 17:56:00 -0600

    Add `repostAuthor` to `Post`

--- Sources/Model/Post.swift
+++ Sources/Model/Post.swift
@@ -47,6 +47,9 @@ public final class Post: JSONAble, Authorable {
     public var author: User? {
         return ElloLinkedStore.sharedInstance.getObject(self.authorId, inCollection: MappingType.UsersType.rawValue) as? User
     }
+    public var repostAuthor: User? {
+        return getLinkObject("repost_author") as? User
+    }
     // nested resources
     public var comments: [Comment]? {
         return getLinkArray(MappingType.CommentsType.rawValue) as? [Comment]
