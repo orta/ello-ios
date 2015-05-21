cb7fd488cb64812c797fff1ad84681876fea621d
Gordon Fontenot <gordon@thoughtbot.com>
2015-05-21 14:29:33 -0400

    Add NotificationBanner to wrap CRToast
    This will act as our interface for the CRToast library

---
+++ Sources/Views/NotificationBanner.swift
@@ -0,0 +1,39 @@
+//
+//  NotificationBanner.swift
+//  Ello
+//
+//  Created by Gordon Fontenot on 5/21/15.
+//  Copyright (c) 2015 Ello. All rights reserved.
+//
+
+import CRToast
+
+public struct NotificationBanner {
+    public static func displayAlertForPayload(payload: PushPayload) {
+        configureDefaults()
+        CRToastManager.showNotificationWithMessage("Hello World") { }
+    }
+}
+
+private extension NotificationBanner {
+    static func configureDefaults() {
+        CRToastManager.setDefaultOptions(
+            [
+                kCRToastNotificationTypeKey: CRToastType.NavigationBar.rawValue,
+                kCRToastNotificationPresentationTypeKey: CRToastPresentationType.Cover.rawValue,
+
+                kCRToastTextColorKey: UIColor.whiteColor(),
+                kCRToastBackgroundColorKey: UIColor.blackColor(),
+
+                kCRToastAnimationOutDirectionKey: CRToastAnimationDirection.Top.rawValue,
+
+                kCRToastAnimationInTimeIntervalKey: DefaultAnimationDuration,
+                kCRToastAnimationOutTimeIntervalKey: DefaultAnimationDuration,
+
+                kCRToastFontKey: UIFont.typewriterFont(12.0),
+                kCRToastTextAlignmentKey: NSTextAlignment.Left.rawValue,
+            ]
+        )
+
+    }
+}
