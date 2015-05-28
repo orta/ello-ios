1e481b53b3e568d33b4a14917f2243189400633f
Sean Dougherty <seancdougherty@gmail.com>
2015-05-27 23:12:46 -0600

    Add TwoLineButton (used in ProfileHeaderCell)

---
+++ Sources/Controllers/Profile/TwoLineButton.swift
@@ -0,0 +1,66 @@
+//
+//  TwoLineButton.swift
+//  Ello
+//
+//  Created by Sean on 5/27/15.
+//  Copyright (c) 2015 Ello. All rights reserved.
+//
+
+public class TwoLineButton: UIButton {
+
+    public var title: String = "" {
+        didSet { updateText() }
+    }
+
+    public var count: String = "" {
+        didSet { updateText() }
+    }
+
+    required override public init(frame: CGRect) {
+        super.init(frame: frame)
+        self.sharedSetup()
+    }
+
+    required public init(coder: NSCoder) {
+        super.init(coder: coder)
+        self.sharedSetup()
+    }
+
+    func sharedSetup() {
+        titleLabel?.numberOfLines = 0
+        backgroundColor = .whiteColor()
+        contentHorizontalAlignment = .Left
+    }
+
+    // MARK: Private
+
+    private func attributes(color: UIColor, font: UIFont) -> [NSObject : AnyObject] {
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 10
+        paragraphStyle.alignment = .Left
+
+        return [
+            NSFontAttributeName : font,
+            NSForegroundColorAttributeName : color,
+            NSParagraphStyleAttributeName : paragraphStyle
+        ]
+    }
+
+    private func updateText() {
+        let countNormalAttributes = attributes(UIColor.blackColor(), font: UIFont.regularBoldFont(12.0))
+        let countSelectedAttributes = attributes(UIColor.greyA(), font: UIFont.regularBoldFont(12.0))
+
+        let titleNormalAttributes = attributes(UIColor.greyA(), font: UIFont.typewriterFont(12.0))
+        let titleSelectedAttributes = attributes(UIColor.greyE5(), font: UIFont.typewriterFont(12.0))
+
+        let attributedNormalCount = NSAttributedString(string: count + "\n", attributes: countNormalAttributes)
+        let attributedSelectedCount = NSAttributedString(string: count + "\n", attributes: countSelectedAttributes)
+
+        let attributedNormalTitle = NSAttributedString(string: title, attributes: titleNormalAttributes)
+        let attributedSelectedTitle = NSAttributedString(string: title, attributes: titleSelectedAttributes)
+
+        setAttributedTitle(attributedNormalCount + attributedNormalTitle, forState: .Normal)
+        setAttributedTitle(attributedSelectedCount + attributedSelectedTitle, forState: .Highlighted)
+    }
+
+}
