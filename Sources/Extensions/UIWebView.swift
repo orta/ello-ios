99dd08e08d82d692c1f0a676a9be18bd4fdf26a4
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2015-06-10 14:59:31 -0600

    Fix issue of links at end of post getting cut off.

--- Sources/Extensions/UIWebView.swift
+++ Sources/Extensions/UIWebView.swift
@@ -10,7 +10,7 @@ public extension UIWebView {
 
     func windowContentSize() -> CGSize? {
         if let jsWidth = self.stringByEvaluatingJavaScriptFromString("(document.getElementById('post-container') || document.getElementByTagName('div')[0] || document.body).offsetWidth") {
-            if let jsHeight = self.stringByEvaluatingJavaScriptFromString("(document.getElementById('post-container') || document.getElementByTagName('div')[0] || document.body).offsetHeight") {
+            if let jsHeight = self.stringByEvaluatingJavaScriptFromString("window.contentHeight()") {
                 let width = CGFloat((jsWidth as NSString).doubleValue)
                 let height = CGFloat((jsHeight as NSString).doubleValue)
                 return CGSize(width: width, height: height)
