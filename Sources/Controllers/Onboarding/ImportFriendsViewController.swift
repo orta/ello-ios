179b9c14c6ef02d9260b7282b02c1b5f072dc5d4
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2015-06-12 11:23:26 -0600

    Update on boarding step to have invite.

--- Sources/Controllers/Onboarding/ImportFriendsViewController.swift
+++ Sources/Controllers/Onboarding/ImportFriendsViewController.swift
@@ -26,26 +26,65 @@ public class ImportFriendsViewController: OnboardingUserListViewController, Onbo
 
     override func setupStreamController() {
         super.setupStreamController()
+        let noResultsTitle = NSLocalizedString("Find your friends!", comment: "Import friends no results title")
+        let noResultsBody = NSLocalizedString("Thanks. We didn’t find any of your friends.\n\nWhen your friends join Ello you’ll be able to find and invite them on the Discover and Search screen.", comment: "Import friends no results body.")
+        streamViewController.noResultsMessages = (title: noResultsTitle, body: noResultsBody)
+        streamViewController.initialLoadClosure = findFriendsFromContacts
+    }
 
+    private func findFriendsFromContacts() {
         var contacts = [String: [String]]()
         for person in addressBook.localPeople {
             contacts[person.identifier] = person.emails
         }
-
-        streamViewController.streamKind = .UserList(endpoint: .FindFriends(contacts: contacts), title: "Find Friends")
-        let noResultsTitle = NSLocalizedString("Find your friends!", comment: "Import friends no results title")
-        let noResultsBody = NSLocalizedString("Thanks. We didn’t find any of your friends.\n\nWhen your friends join Ello you’ll be able to find and invite them on the Discover and Search screen.", comment: "Import friends no results body.")
-        streamViewController.noResultsMessages = (title: noResultsTitle, body: noResultsBody)
+        InviteService().find(contacts,
+            currentUser: self.currentUser,
+            success: { users in
+                self.streamViewController.clearForInitialLoad()
+                let userIdentifiers = users.map { $0.identifiableBy ?? "" }
+                let mixed: [(LocalPerson, User?)] = self.addressBook.localPeople.map {
+                    if let index = find(userIdentifiers, $0.identifier) {
+                        return ($0, users[index])
+                    }
+                    return ($0, .None)
+                }
+                self.setContacts(mixed)
+            },
+            failure: { _ in
+                let contacts: [(LocalPerson, User?)] = self.addressBook.localPeople.map { ($0, .None) }
+                self.setContacts(contacts)
+                self.streamViewController.doneLoading()
+            }
+        )
     }
 
-    override func usersLoaded(users: [User]) {
-        let filteredUsers = InviteService.filterUsers(users, currentUser: currentUser)
+    public func setContacts(contacts: [(LocalPerson, User?)]) {
+        var foundItems = [StreamCellItem]()
+        var inviteItems = [StreamCellItem]()
+        for contact in contacts {
+            var (person: LocalPerson, user: User?) = contact
+            if user != nil {
+                foundItems.append(StreamCellItem(jsonable: user!, type: StreamCellType.UserListItem, data: nil, oneColumnCellHeight: 56.0, multiColumnCellHeight: 56.0, isFullWidth: true))
+            }
+            else {
+                if let currentUser = currentUser, let profile = currentUser.profile {
+                    if !contains(person.emails, profile.email) {
+                        inviteItems.append(StreamCellItem(jsonable: person, type: StreamCellType.InviteFriends, data: nil, oneColumnCellHeight: 56.0, multiColumnCellHeight: 56.0, isFullWidth: true))
+                    }
+
+                }
+            }
+        }
+        foundItems.sort { ($0.jsonable as! User).username.lowercaseString < ($1.jsonable as! User).username.lowercaseString }
+        inviteItems.sort { ($0.jsonable as! LocalPerson).name.lowercaseString < ($1.jsonable as! LocalPerson).name.lowercaseString }
+        users = foundItems.map { $0.jsonable as! User }
+        let filteredUsers = InviteService.filterUsers(users!, currentUser: currentUser)
         let header = NSLocalizedString("Find your friends!", comment: "Find Friends Header text")
         let message = NSLocalizedString("Use your address book to find and invite your friends on Ello.", comment: "Find Friends Description text")
         appendHeaderCellItem(header: header, message: message)
         appendFollowAllCellItem(userCount: count(filteredUsers))
-
-        super.usersLoaded(filteredUsers)
+        // this calls doneLoading when cells are added
+        streamViewController.appendUnsizedCellItems(foundItems + inviteItems, withWidth: self.view.frame.width)
     }
 
 }
