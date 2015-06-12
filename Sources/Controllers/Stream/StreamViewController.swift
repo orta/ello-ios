fab029ce42ddab1af35f5728a45ea6639d8cb566
Sean Dougherty <seancdougherty@gmail.com>
2015-06-12 17:29:22 -0600

    Optimize initial api loads

--- Sources/Controllers/Stream/StreamViewController.swift
+++ Sources/Controllers/Stream/StreamViewController.swift
@@ -198,6 +198,7 @@ public class StreamViewController: BaseElloViewController {
     }
 
     public func doneLoading() {
+//        println("---------PROFILING: StreamVC-\(streamKind.name) initial finished sizing: \(NSDate().timeIntervalSinceDate(LaunchDate))")
         ElloHUD.hideLoadingHudInView(view)
         pullToRefreshView?.finishLoading()
         initialDataLoaded = true
@@ -266,6 +267,7 @@ public class StreamViewController: BaseElloViewController {
     public var initialLoadClosure: ElloEmptyCompletion?
 
     public func loadInitialPage() {
+//        println("---------PROFILING: StreamVC-\(streamKind.name) loadInitialPage: \(NSDate().timeIntervalSinceDate(LaunchDate))")
         if let initialLoadClosure = initialLoadClosure {
             initialLoadClosure()
         }
@@ -277,6 +279,7 @@ public class StreamViewController: BaseElloViewController {
                 streamKind.endpoint,
                 streamKind: streamKind,
                 success: { (jsonables, responseConfig) in
+//                    println("---------PROFILING: StreamVC-\(self.streamKind.name) initialpageLoaded: \(NSDate().timeIntervalSinceDate(LaunchDate))")
                     if self.loadInitialPageLoadingToken != localToken { return }
                     self.clearForInitialLoad()
                     self.responseConfig = responseConfig
