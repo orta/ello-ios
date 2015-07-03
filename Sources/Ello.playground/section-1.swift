93914cec7de0d4700519493a33c1dc2fc3fe2853
Sean Dougherty <seancdougherty@gmail.com>
2015-07-03 11:22:25 -0600

    update playground

--- Sources/Ello.playground/section-1.swift
+++ Sources/Ello.playground/section-1.swift
@@ -4,3 +4,22 @@ import Foundation
 import UIKit
 
 
+func findUsername(text: String) -> (Bool, String?) {
+    if text.rangeOfString("\\s?@{1}\\w+", options: .RegularExpressionSearch) != nil {
+        return (true, text)
+    }
+    else {
+        return (false, nil)
+    }
+}
+
+func wordAtIndex(text: String, range: Range<String.Index>) -> String? {
+    var range = text.rangeOfString(".", options: .BackwardsSearch)?.startIndex
+    println(range)
+    return nil
+}
+
+wordAtIndex("ss.ss")
+
+findUsername("blah")
+findUsername("@blah")
