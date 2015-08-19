6e3877312d79847200520d24a698e43ed433c34e
Colin T.A. Gray <colinta@gmail.com>
2015-08-19 14:43:25 -0600

    inform user when toggling omnibar editor

--- Sources/Controllers/App/DebugTodoController.swift
+++ Sources/Controllers/App/DebugTodoController.swift
@@ -67,12 +67,18 @@ class DebugTodoController: UIViewController, UITableViewDataSource, UITableViewD
             Onboarding.shared().reset()
         }
         addAction("Toggle New Omnibar (multiple regions)") {
+            let alert: String
             if Defaults["OmnibarNewEditorEnabled"].bool ?? false {
                 Defaults["OmnibarNewEditorEnabled"] = false
+                alert = "New Omnibar disabled"
             }
             else {
                 Defaults["OmnibarNewEditorEnabled"] = true
+                alert = "New Omnibar enabled"
             }
+
+            let alertController = AlertViewController(message: alert)
+            self.presentViewController(alertController, animated: true, completion: nil)
         }
         addAction("Crash the app") {
             Crashlytics.sharedInstance().crash()
