33ecefb3fc2ec806808bf818aa4c350e445a7ecd
Colin T.A. Gray <colinta@gmail.com>
2015-09-17 14:32:11 -0600

    move Regex extensions out and test them

--- Sources/Utilities/Tag.swift
+++ Sources/Utilities/Tag.swift
@@ -1,58 +1,6 @@
 import Foundation
 import UIKit
 
-class Regex {
-    let regex: NSRegularExpression!
-    let pattern: String
-
-    init?(_ pattern: String) {
-        self.pattern = pattern
-        var error: NSError?
-        do {
-            self.regex = try NSRegularExpression(pattern: pattern, options: NSRegularExpressionOptions(rawValue: 0))
-        } catch let error1 as NSError {
-            error = error1
-            self.regex = nil
-        }
-        if error != nil { return nil }
-    }
-
-    func test(input: String) -> Bool {
-        return match(input) != nil
-    }
-
-    func match(input: String) -> String? {
-        if let range = input.rangeOfString(pattern, options: .RegularExpressionSearch) {
-            return input.substringWithRange(range)
-        }
-        return nil
-    }
-
-    func matches(input: String) -> [String] {
-        let nsstring = input as NSString
-        let matches = self.regex.matchesInString(input, options: [], range: NSRange(location: 0, length: nsstring.length)) 
-        var ret = [String]()
-        for match in matches {
-            for i in 0..<match.numberOfRanges {
-                let range = match.rangeAtIndex(i)
-                ret.append(nsstring.substringWithRange(range))
-            }
-            break
-        }
-        return ret
-    }
-
-}
-
-infix operator =~ {}
-infix operator ~ {}
-func =~ (input: String, pattern: String) -> Bool {
-    return Regex(pattern)!.test(input)
-}
-func ~ (input: String, pattern: String) -> String? {
-    return Regex(pattern)!.match(input)
-}
-
 
 let PreserveWs = [
     "style",
