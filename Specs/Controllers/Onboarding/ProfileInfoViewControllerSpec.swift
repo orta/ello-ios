ac4ab2b997ce3ed9a0405aa49d1f34ce5a4ef5a8
Colin T.A. Gray <colinta@gmail.com>
2015-11-04 15:53:39 -0700

    fixes onboarding step 'ProfileInfoViewController'
    correct margins
    updated snapshot specs

--- Specs/Controllers/Onboarding/ProfileInfoViewControllerSpec.swift
+++ Specs/Controllers/Onboarding/ProfileInfoViewControllerSpec.swift
@@ -47,6 +47,21 @@ class ProfileInfoViewControllerSpec: QuickSpec {
                     }
                 }
             }
+            context("iPad in portrait multitasking") {
+                beforeEach {
+                    let parent = UIView(frame: CGRect(x: 0, y: 0, width: 438, height: 1024))
+                    subject.view.frame = parent.bounds
+                    parent.addSubview(subject.view)
+                    subject.view.layoutIfNeeded()
+                    showView(parent)
+                }
+                describe("view") {
+                    it("should match the screenshot") {
+                        let view = subject.view
+                        expect(view).to(haveValidSnapshot())
+                    }
+                }
+            }
             context("iPhone5 in portrait") {
                 beforeEach {
                     let parent = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 568))
