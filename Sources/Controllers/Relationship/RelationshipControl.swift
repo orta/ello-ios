52b04a3733852d5cfa73fca49634dacdb79f6271
Colin T.A. Gray <colinta@gmail.com>
2015-11-16 11:25:41 -0700

    use InterfaceString for Starred title

--- Sources/Controllers/Relationship/RelationshipControl.swift
+++ Sources/Controllers/Relationship/RelationshipControl.swift
@@ -257,8 +257,7 @@ public class RelationshipControl: UIView {
             switch self {
             case .None: return NSLocalizedString("Follow", comment: "Follow button title")
             case .Following: return InterfaceString.Following.Title.localized
-//            case .Starred: return InterfaceString.Starred.Title.localized
-            case .Starred: return NSLocalizedString("Starred", comment: "Starred button title")
+            case .Starred: return InterfaceString.Starred.Title.localized
             case .Muted: return NSLocalizedString("Muted", comment: "Muted button title")
             }
         }
