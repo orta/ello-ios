bf5e54ea511cb4555305e2f3e3c205a4994594b0
Colin T.A. Gray <colinta@gmail.com>
2015-12-11 14:26:23 -0700

    bring back the underline on the 'TwoLineButton'

--- Sources/Controllers/Profile/TwoLineButton.swift
+++ Sources/Controllers/Profile/TwoLineButton.swift
@@ -34,7 +34,7 @@ public class TwoLineButton: UIButton {
 
     // MARK: Private
 
-    private func attributes(color: UIColor, font: UIFont) -> [String : AnyObject] {
+    private func attributes(color: UIColor, font: UIFont, underline: Bool = false) -> [String : AnyObject] {
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.lineSpacing = 5
         paragraphStyle.alignment = .Left
@@ -43,6 +43,7 @@ public class TwoLineButton: UIButton {
             NSFontAttributeName : font,
             NSForegroundColorAttributeName : color,
             NSParagraphStyleAttributeName : paragraphStyle,
+            NSUnderlineStyleAttributeName : (underline ? NSUnderlineStyle.StyleSingle.rawValue : NSUnderlineStyle.StyleNone.rawValue)
         ]
     }
 
@@ -50,8 +51,8 @@ public class TwoLineButton: UIButton {
         let countNormalAttributes = attributes(UIColor.blackColor(), font: UIFont.defaultBoldFont())
         let countSelectedAttributes = attributes(UIColor.greyA(), font: UIFont.defaultBoldFont())
 
-        let titleNormalAttributes = attributes(UIColor.greyA(), font: UIFont.defaultFont())
-        let titleSelectedAttributes = attributes(UIColor.greyE5(), font: UIFont.defaultFont())
+        let titleNormalAttributes = attributes(UIColor.greyA(), font: UIFont.defaultFont(), underline: true)
+        let titleSelectedAttributes = attributes(UIColor.greyE5(), font: UIFont.defaultFont(), underline: true)
 
         let attributedNormalCount = NSAttributedString(string: count + "\n", attributes: countNormalAttributes)
         let attributedSelectedCount = NSAttributedString(string: count + "\n", attributes: countSelectedAttributes)
