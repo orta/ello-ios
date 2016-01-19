0e813313197cea7fdddf71dff1ec5784b45d182f
Colin T.A. Gray <colinta@gmail.com>
2016-01-19 11:46:34 -0700

    adds the ReauthenticationSpec, uses RecordedResponse to handle fake responses

--- Sources/Networking/ElloProvider_Specs.swift
+++ Sources/Networking/ElloProvider_Specs.swift
@@ -8,6 +8,17 @@
 
 import Moya
 
+public struct RecordedResponse {
+    let endpoint: ElloAPI
+    let response: EndpointSampleResponse
+
+    public init(endpoint: ElloAPI, response: EndpointSampleResponse) {
+        self.endpoint = endpoint
+        self.response = response
+    }
+
+}
+
 
 public struct ElloProvider_Specs {
     public static var errorStatusCode: ErrorStatusCode = .Status404
@@ -21,6 +32,35 @@ public struct ElloProvider_Specs {
         let parameters = target.parameters
         let endpoint = Endpoint<ElloAPI>(URL: url(target), sampleResponseClosure: sampleResponseClosure, method: method, parameters: parameters)
         return endpoint.endpointByAddingHTTPHeaderFields(target.headers)
+    }
+
+    static func recordedEndpointsClosure(recordings: [RecordedResponse]) -> (target: ElloAPI) -> Endpoint<ElloAPI> {
+        var playback = recordings
+        return { (target: ElloAPI) -> Endpoint<ElloAPI> in
+            var response: EndpointSampleResponse? = nil
+            for (index, recording) in playback.enumerate() {
+                if recording.endpoint.description == target.description {
+                    response = recording.response
+                    playback.removeAtIndex(index)
+                    break
+                }
+            }
+
+            let sampleResponseClosure: () -> EndpointSampleResponse
+            if let response = response {
+                sampleResponseClosure = { return response }
+            }
+            else {
+                sampleResponseClosure = {
+                    return EndpointSampleResponse.NetworkResponse(200, target.sampleData)
+                }
+            }
+
+            let method = target.method
+            let parameters = target.parameters
+            let endpoint = Endpoint<ElloAPI>(URL: url(target), sampleResponseClosure: sampleResponseClosure, method: method, parameters: parameters)
+            return endpoint.endpointByAddingHTTPHeaderFields(target.headers)
+        }
     }
 
 }
@@ -38,6 +78,10 @@ extension ElloProvider {
 
     public static func ErrorStubbingProvider() -> MoyaProvider<ElloAPI> {
         return MoyaProvider<ElloAPI>(endpointClosure: ElloProvider_Specs.errorEndpointsClosure, stubClosure: MoyaProvider.ImmediatelyStub)
+    }
+
+    public static func RecordedStubbingProvider(recordings: [RecordedResponse]) -> MoyaProvider<ElloAPI> {
+        return MoyaProvider<ElloAPI>(endpointClosure: ElloProvider_Specs.recordedEndpointsClosure(recordings), stubClosure: MoyaProvider.ImmediatelyStub)
     }
 
 }
