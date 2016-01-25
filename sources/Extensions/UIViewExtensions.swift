3edf84ce70c606591a8e4ad8e7fb5842c4e10f60
Sean Dougherty <seancdougherty@gmail.com>
2016-01-25 13:22:51 -0700

    WIP: adjust search toggle button sizes

---
+++ sources/Extensions/UIViewExtensions.swift
@@ -0,0 +1,13 @@
+//
+//  UIViewExtensions.swift
+//  Ello
+//
+//  Created by Sean on 1/25/16.
+//  Copyright © 2016 Ello. All rights reserved.
+//
+
+extension UIView {
+    public func addToView(view: UIView) {
+        view.addSubview(self)
+    }
+}
