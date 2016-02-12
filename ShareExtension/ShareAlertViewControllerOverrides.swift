4f0ad1b98d0a7e3e5a5a3fa6866a454ef3b124cc
Sean Dougherty <seancdougherty@gmail.com>
2016-02-12 13:51:34 -0700

    turn off zombies

--- ShareExtension/ShareAlertViewControllerOverrides.swift
+++ ShareExtension/ShareAlertViewControllerOverrides.swift
@@ -11,7 +11,7 @@ import Foundation
 public extension AlertViewController {
     // do not reference anything in the Keyboard
     // App Extensions are prohibited from using
-    // some API
+    // some APIs
     func keyboardUpdateFrame(keyboard: Keyboard) {
     }
 }
