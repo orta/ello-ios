a567fc7dcf91da8c4aac9b5d876e448f5bf2c988
Colin T.A. Gray <colinta@gmail.com>
2016-02-24 13:21:29 -0700

    adds specs for NotificationAttributedTitle

--- Specs/Model/NotificationAttributedTitleSpec.swift
+++ Specs/Model/NotificationAttributedTitleSpec.swift
@@ -6,6 +6,7 @@
 //  Copyright (c) 2015 Ello. All rights reserved.
 //
 
+@testable
 import Ello
 import Quick
 import Nimble
@@ -13,18 +14,32 @@ import Nimble
 
 class NotificationAttributedTitleSpec: QuickSpec {
     override func spec() {
-        describe("creating an attributed string") {
-            xit("supports RepostNotification") {}
-            xit("supports NewFollowedUserPost") {}
-            xit("supports NewFollowerPost") {}
-            xit("supports PostMentionNotification") {}
-            xit("supports CommentMentionNotification") {}
-            xit("supports InvitationAcceptedPost") {}
-            xit("supports CommentNotification") {}
-            xit("supports CommentOnOriginalPostNotification") {}
-            xit("supports CommentOnRepostNotification") {}
-            xit("supports WelcomeNotification") {}
-            xit("supports missing author") {}
+        describe("NotificationAttributedTitle") {
+            describe("attributedTitle(_: Activity.Kind, author: User?, subject: JSONAble?)") {
+                let user: User = stub(["username": "ello"])
+                let post: Post = stub([:])
+                let comment: ElloComment = stub(["parentPost": post])
+                let expectations: [(Activity.Kind, JSONAble?, String)] = [
+                    (.RepostNotification, post, "@ello reposted your post."),
+                    (.NewFollowedUserPost, post, "You started following @ello."),
+                    (.NewFollowerPost, user, "@ello started following you."),
+                    (.PostMentionNotification, post, "@ello mentioned you in a post."),
+                    (.CommentNotification, comment, "@ello commented on your post."),
+                    (.CommentMentionNotification, comment, "@ello mentioned you in a comment."),
+                    (.CommentOnOriginalPostNotification, comment, "@ello commented on your post"),
+                    (.CommentOnRepostNotification, comment, "@ello commented on your repost."),
+                    (.InvitationAcceptedPost, user, "@ello accepted your invitation."),
+                    (.LoveNotification, post, "@ello loved your post."),
+                    (.LoveOnRepostNotification, post, "@ello loved your repost."),
+                    (.LoveOnOriginalPostNotification, post, "@ello loved a repost of your post."),
+                    (.WelcomeNotification, nil, "Welcome to Ello!"),
+                ]
+                for (activityKind, subject, string) in expectations {
+                    it("supports \(activityKind)") {
+                        expect(NotificationAttributedTitle.attributedTitle(activityKind, author: user, subject: subject).string) == string
+                    }
+                }
+            }
         }
     }
 }
