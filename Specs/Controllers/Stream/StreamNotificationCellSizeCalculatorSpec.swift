1c91e0644db942937732ab82c4c87c7fd35ff58a
Colin T.A. Gray <colinta@gmail.com>
2016-05-06 11:59:52 -0500

    don't focus specs, shorten dummy text

--- Specs/Controllers/Stream/StreamNotificationCellSizeCalculatorSpec.swift
+++ Specs/Controllers/Stream/StreamNotificationCellSizeCalculatorSpec.swift
@@ -29,9 +29,9 @@ class StreamNotificationCellSizeCalculatorSpec : QuickSpec {
     }
 
     override func spec() {
-        fdescribe("StreamNotificationCellSizeCalculator") {
+        describe("StreamNotificationCellSizeCalculator") {
             let user: User = stub([:])
-            let text: TextRegion = stub(["content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consectetur molestie faucibus. Phasellus iaculis pellentesque felis eu fringilla. Ut in sollicitudin nisi. Praesent in mauris tortor. Nam interdum, magna eu pellentesque scelerisque, dui ipsum adipiscing ante, vel ullamcorper nisl sapien id arcu. Nullam egestas diam eu felis mollis sit amet cursus enim vehicula. Quisque eu tellus id erat pellentesque consequat. Maecenas fermentum faucibus magna, eget dictum nisi congue sed. Quisque a justo a nisi eleifend facilisis sit amet at augue. Sed a sapien vitae augue hendrerit porta vel eu ligula. Proin enim urna, faucibus in vestibulum tincidunt, commodo sit amet orci. Vestibulum ac sem urna, quis mattis urna. Nam eget ullamcorper ligula. Nam volutpat, arcu vel auctor dignissim, tortor nisi sodales enim, et vestibulum nulla dui id ligula. Nam ullamcorper, augue ut interdum vulputate, eros mauris lobortis sapien, ac sodales dui eros ac elit."])
+            let text: TextRegion = stub(["content": "Lorem ipsum dolor sit amet."])
             let image: ImageRegion = stub(["asset": Asset.stub(["attachment": Attachment.stub(["width": 2000, "height": 2000])])])
             let postWithText: Post = stub(["summary": [text], "content": [text], "author": user])
             let postWithImage: Post = stub(["summary": [image], "content": [image], "author": user])
